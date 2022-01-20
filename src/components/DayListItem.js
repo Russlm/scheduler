1a2db054a3aff96fc4cc6e6276596e52b8503981
Russell <russell.e.mair@gmail.com>
2022-01-20 18:20:03 -0500

    adds DayListItem, a component that displays whether a particular day in the scheduler has space for more events. This commit then adds css support to the DayListItem and adds className utility to the DayListItem to manage class css more efficiently.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,20 @@
 import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots 
+  });
+
   return (
-    <li>
+    <li 
+      className={dayClass}
+    >
       <h2 className="text--regular">Day Name</h2> 
       <h3 className="text--light">X spots remaining</h3>
     </li>
   );
 }
+
