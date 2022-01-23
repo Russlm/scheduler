aba127575d19b454ac71189c190cc5c57bb146c0
Russell <russell.e.mair@gmail.com>
2022-01-23 15:26:32 -0500

    updates DayList component to dynamically create array of DayListItem component. DayList Story works.

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,7 +2,36 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  return (
-    <ul></ul>
+  const listItem = props.days.map( x => (
+    <DayListItem 
+    key={x.id}
+    name={x.name} 
+    spots={x.spots} 
+    selected={x.name === props.day}
+    setDay={x.setDay}  
+  />
+  ))
+  return(
+    <ul>
+         {listItem}  
+    </ul>
   )
 };
+
+
+
+
+
+// FIRST ATTEMPT 
+// // return (
+//   <ul>{props.days.map( x => {
+//     <ul>
+//     <li>{x.id}</li>
+//     <li>{x.name}</li>
+//     <li>{x.days === props.day}</li>
+//     <li>{x.setDay}</li>
+//     </ul>
+//   }
+    
+//     )}</ul>
+// )
