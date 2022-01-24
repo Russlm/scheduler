b0421143315a47e0ee72e6cc337935621b8749a8
Russell <russell.e.mair@gmail.com>
2022-01-24 01:33:27 -0500

    adds HTML and hardcoded data into InterviewerListItem

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -2,3 +2,13 @@ import React from "react";
 
 import { useState } from "react";
 
+export default function InterviewerListItem(props) { 
+  <li className="interviewers__item">
+  <img
+    className="interviewers__item-image"
+    src="https://i.imgur.com/LpaY82x.png"
+    alt="Sylvia Palmer"
+  />
+  Sylvia Palmer
+</li>
+}
