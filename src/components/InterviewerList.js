e9b365c26b08216b93bc113b1cec4425a3d4f23c
Russell <russell.e.mair@gmail.com>
2022-01-24 03:27:58 -0500

    InterviewList now correctly passes setInterviewer on click.

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -9,7 +9,7 @@ export default function InterviewerList(props) {
       name={interviewer.name} 
       avatar={interviewer.avatar} 
       selected={interviewer.id === props.interviewer}
-      setDay={() => props.setInterviewer(interviewer.id)}  
+      setInterviewer={() => props.setInterviewer(interviewer.id)}  
     />
   ));
   return (
