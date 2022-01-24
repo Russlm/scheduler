0cdfdd7bbe7052bdaafc6111bd85f4fe54f85e64
Russell <russell.e.mair@gmail.com>
2022-01-24 01:36:46 -0500

    makes some corrections to prev 2 commits. adds return statement to InterviewerListItem and imports InterviewerListItem to Storybook.

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,12 +3,12 @@ import React from "react";
 import { useState } from "react";
 
 export default function InterviewerListItem(props) { 
-  <li className="interviewers__item">
+  return (<li className="interviewers__item">
   <img
     className="interviewers__item-image"
     src="https://i.imgur.com/LpaY82x.png"
     alt="Sylvia Palmer"
   />
   Sylvia Palmer
-</li>
+</li>)
 }
