bd3fa54a7c9833e4e4b5655c174ff16b0ceb0679
Russell <russell.e.mair@gmail.com>
2022-01-24 08:56:22 -0500

    creates Empty element, adds story to storybook and confirms working onClick functionality.

--- stories/index.js
+++ stories/index.js
@@ -11,7 +11,8 @@ import DayList from "components/DayList"
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
-import Header from "components/Appointment/header";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty"
 
 
 storiesOf("Button", module)
@@ -150,3 +151,4 @@ storiesOf("Button", module)
           time={"12pm"}
           />
       ))
+      .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
