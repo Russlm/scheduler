7ff65aba245dcc9f3fb44579410eeb215fd38988
Russell <russell.e.mair@gmail.com>
2022-01-25 22:44:00 -0500

    updates the interviewList and Daylist items to take in state variables value and onChange when updating information. updates stories to reflect this change.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,6 +5,8 @@ import { useState } from "react";
 
 
 export default function Form(props) { 
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -14,7 +16,8 @@ export default function Form(props) {
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            value= {props.student}
+            value= {student}
+            onChange={event => setStudent(event.target.value)}
           />
         </form>
         <InterviewerList 
